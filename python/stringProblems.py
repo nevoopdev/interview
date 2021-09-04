a08f0f467b54e82beb348b0aa4a64c33f862bca6
POOVEN <nevoopdev@gmail.com.com>
2021-09-04 17:17:16 +0530

    string problems updated

--- python/stringProblems.py
+++ python/stringProblems.py
@@ -77,20 +77,152 @@ def reverseWithPreserve(str):
             list[j] = str[i]
             j -=1
     str1 = ""
+    print(str1.join(list))  
+# reverseWithPreserve("i am string")
+
+
+# 6) REVERSE A WORD IN A GIVEN STRING 
+# eg :  hello world -> olleh dlrow
+
+def reverseWord(str):
+    str = str.split(" ")
+    list = []
+    
+    for i in range(0,len(str)):
+        list.append(str[i][::-1])
+    str1 = " "
     print(str1.join(list))
-   
 
-        
+# reverseWord("hello world")
+
+# 7) Program to print first non repeated character from String
+
+def nonRepeatChar(str):
     
-reverseWithPreserve("poovven is some")
+    for i in range(0,len(str)):
+        count = False
+
+        for j in range(0,len(str)):
+            if(str[i] == str[j]) & (i !=j):
+                count  = True
+                break
+        if(count == False):
+            return str[i]
+
+    return "no"
+            
+# print(nonRepeatChar("aaabbccddffpg"))
+
+
+def nonrepeating(str):
+
+    counts = {}
+    order = []
+
+    for i in str:
+        if i in counts:
+            counts[i] +=1
+            
+        else:
+            counts[i] = 1
+            order.append(i)
+    for j in order:
+        if counts[j] == 1:
+          return j
+    return None
+    
+# print(nonrepeating("helooooopphel"))
+
+
+# 7) b) Program to print first non repeated character from String using WHILE LOOP
+
+
+def demo(str):
+
+    while str !="":
+        len1 = len(str)
+        char = str[0]
+        str = str.replace(char,"")
+        len2 = len(str)
+
+        if len2 == len1-1:
+            print(" the first unique char is ",char)
+            break
+        else:
+            print("no unique char")
+# demo("pooven")
 
 
 
+# 8) Check whether the given string is only contains numbers
+
+import re
+from typing import Counter
+
+def findonlydigits(str):
+    str1 = str.split(" ")
+    str2 = "".join(str1)  #this is process to delete all empty spaces in the string
+    print(str2)
+    newstr = re.findall("[0-9]",str2)
+    if len(str2) == len(newstr):
+        print("this string contains only digits")
+    else:
+        print("this is not digits")
+
+# findonlydigits("0009")
 
 
+# 9) reverse String in Java using Iteration and Recursion?
+
+def reversestr(str):
+    newstr = ""
+    for i in str:
+        newstr = i + str
+    print("reverse string using iteration :",end=" ")
+    print(newstr)
+# reversestr("hello world exexcuse me")
+
+def reversestr1(str):
+    if(len(str)==0):
+        return str
+    else:
+        return reversestr1(str[1:]) + str[0]
+
+# print(reversestr1("hello world"))
+
+# 10) find duplicate characters 
+
+def dupChar(str):
+
+    counts = {}
+    for i in str:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+            
+    
+    for i in counts:
+        if counts[i] > 1:
+            print(i ,end=":")
+            print(counts[i])
+
+# dupChar("poovendhan")
+
+# 11) count vowels and consonants in a given string
+import re
+def vowelcounter(str):
+    vowel = 0
+    consonsnts = 0
+    str = str.replace(" ","")
+    print(str)
+
+    count = re.findall("[aeiouAEIOU]",str)
+    vowel = len(count)
+    consonsnts = len(str) - vowel
+
+    print("vowels :",vowel)
+    print("consonants :",consonsnts)
     
 
-     
-    
- 
-
+vowelcounter("nithees kumar")
